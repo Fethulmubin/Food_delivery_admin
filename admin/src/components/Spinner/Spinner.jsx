c1089becbff23d3105a0a5c4410b0c63352e4183
fethulmubin <fetihul@gmail.com>
2025-03-22 17:28:06 +0300

    text change with timer feature

--- admin/src/components/Spinner/Spinner.jsx
+++ admin/src/components/Spinner/Spinner.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './Spinner.css'
 import burgerSpinner from '../../assets/burger_spin.png'
 
-
 const Spinner = () => {
-    return (
-        <div className="spinner-container">
-            <img src={burgerSpinner} alt="Loading..." className="burger-spinner" />
-        </div>
-    )
-}
+      const [message, setMessage] = useState("Mounting your dashboard...");
+   
+       useEffect(() => {
+         const timer = setTimeout(() => {
+           setMessage("Almost there...");
+         }, 2000); 
+     
+         return () => clearTimeout(timer);
+       }, []);
+       return (
+           <div className="spinner-container">
+               <img src={burgerSpinner} alt="Loading..." className="burger-spinner" />
+               <p>{message}</p>
+   
+           </div>
+       )
+   }
 
 export default Spinner
